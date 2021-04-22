cf3856639525369df00c757501307ace18a929f5
Jeff Pratt <me@jpratt.dev>
2021-04-22 00:36:52 -0600

    Update Main.scala

--- src/main/g8/src/main/scala/$package$/Main.scala
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -1,10 +1,9 @@
 package $package$
 
 import cats.effect._
-import cats.effect.Console.io._
 
 object Main extends IOApp {
   def run(args: List[String]): IO[ExitCode] = {
-    putStrLn("Hello, Cats!").as(ExitCode.Success)
+    IO.println("Hello, Cats!").as(ExitCode.Success)
   }
 }
